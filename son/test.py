ffb7079235668f21720f1877a522edcb0be314e1
Guest <you@example.com>
2018-08-11 22:17:45 +0700

    update last day

--- son/test.py
+++ son/test.py
@@ -42,10 +42,9 @@ def create_board():
 def print_board(board):
     alpha = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
     copyBoard = []
-    for i in board:
-        copyBoard.append(i)
-    print(board)
-    print(copyBoard)
+    copyBoard = list(board)
+    print(id(board))
+    print(id(copyBoard))
     copyBoard[0].insert(0,'1')
     copyBoard[1].insert(0,'2')
     copyBoard[2].insert(0,'3')
@@ -57,7 +56,7 @@ def print_board(board):
     newBoard = '\n'.join([''.join(['{:2}'.format(item) for item in row]) for row in copyBoard])
     print(*alpha)
     print(newBoard)
-    return
+    
 
 def game_loop(board, piece):
     print()
